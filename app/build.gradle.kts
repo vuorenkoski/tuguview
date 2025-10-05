26a3bfb23123a24601a780dd9617f9d342bd37df
Lauri Vuorenkoski <lauri.vuorenkoski@live.fi>
2025-10-05 17:09:50 +0300

    fix to dialog text color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "fi.vuorenkoski.tuguview"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.2"
+        versionCode = 2
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
